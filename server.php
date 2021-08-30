98291e5cdf788cf1dcdf8ceb865943eef468ce2f
IGOR <a15000a60@gmail.com>
2021-08-30 19:16:11 +0400

    start

--- server.php
+++ server.php
@@ -1,3 +1,6 @@
 <?php
+//для получения данных JSON
 $_POST = json_decode(file_get_contents("php://input"),true);
-echo var_dump($_POST);
+
+//для обычных данных
+echo var_dump($_POST);//берет данные с клиента,превращает в строку и показывает нам на клиенте
