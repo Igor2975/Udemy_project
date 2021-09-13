c19bdcabae2f15674668e41eb1bd6d931fc12320
IGOR <a15000a60@gmail.com>
2021-09-13 21:23:20 +0400

    async\await создание карточек на основе данных с сервера

--- js/script.js
+++ js/script.js
@@ -168,37 +168,24 @@ modal.addEventListener('click', (e) => {
             this.parent.append(element);
         }
     }
-   /* const div = new MenuCard(аргументы),
-     div.render();*/
-     //или можно так:
-     new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container',
-        
-     ).render();
 
-     new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        '.menu .container',
-        'menu__item'
-     ).render();
+    const getResourse = async(url) =>{
+        const res =  await fetch(url);
 
-     new MenuCard(
-        "img/tabs/post.jpg",
-        "poste",
-        'Меню "Постное"',
-         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 21,
-        '.menu .container',
-        'menu__item'
-     ).render();
+        if(!res.ok){
+            throw new Error(`Could not fetch : ${url},status ${res.status}`);
+        }
+
+        return await res.json();
+    };
+
+    getResourse('http://localhost:3000/menu')
+       .then(data =>{
+           data.forEach(({img,altimg,title,descr,price}) =>{//деструктурируем obj
+               new MenuCard(img,altimg,title,descr,price, '.menu .container').render();
+           });
+       });
+  
      
      //Forms
 
@@ -211,10 +198,23 @@ modal.addEventListener('click', (e) => {
      };
 
      forms.forEach(item =>{
-         postData(item);
+         bindPostData(item);
      })
+    // async - говорит что в ф-и асинхронный код
+     //await - говорит что надо дождаться ответа 
+     const postData = async(url,data) =>{
+         const res =  await fetch(url, {
+            method:"POST",
+            headers:{
+               'Content-type':'application/json'
+            },
+            body:data
+         });
 
-     function postData(form){
+         return await res.json();
+     };
+
+     function bindPostData(form){
          form.addEventListener('submit', (e) =>{
 
              //отмена перезагрузки браузера
@@ -235,20 +235,10 @@ modal.addEventListener('click', (e) => {
              const formData = new FormData(form);
              // в index.html  в данных всегда должен быть указан атрибут "name"
 
-             const object = {};
-             formData.forEach(function(value,key){
-                 object[key] = value;
-             })         
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));        
 
-             fetch('server.php',{
-                method:"POST",
-                headers:{
-                   'Content-type':'application/json'
-                },
-                //переводим обьект в json
-                body:JSON.stringify(object)
-            }).then(data => data.text())
-
+            postData('http://localhost:3000/requests',json)    
+            
             .then(data =>{
                 console.log(data);
                 showThanksModal(message.success) ;
